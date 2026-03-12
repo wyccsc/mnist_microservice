b1af16fefcad7af25100768c271f187acdb768bf
wyccsc <wyc890619@gmail.com>
2026-03-12 20:21:59 +0800

    all

--- api-service/main.py
+++ api-service/main.py
@@ -5,11 +5,34 @@ app = FastAPI()
 
 MODEL_URL = "http://mnist_microservice_model_service:8000/predict"
 
+RESULT_FILE = "predictions.csv"
+
 @app.post("/upload_predict")
 async def upload_predict(datafile: UploadFile = File(...)):
+
     content = await datafile.read()
+
     response = requests.post(
         MODEL_URL,
         files={"file": (datafile.filename, content)}
     )
-    return response.json()
+
+    result = response.json()
+
+    labels = result["labels"]
+
+    df = pd.DataFrame({"Label": labels})
+
+    df.to_csv(RESULT_FILE,index=False)
+
+    return {"labels":labels}
+
+
+@app.get("/download")
+async def download():
+
+    return FileResponse(
+        path=RESULT_FILE,
+        filename="predictions.csv",
+        media_type="text/csv"
+    )
