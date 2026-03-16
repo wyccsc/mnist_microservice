df65c1f1c3c969224848e928612b3515d8b215f4
wyccsc <wyc890619@gmail.com>
2026-03-16 19:41:42 +0800

    API

--- api-service/main.py
+++ api-service/main.py
@@ -3,28 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import requests
 import pandas as pd
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-MODEL_URL = "http://mnist_microservice_model_service:8000/predict"
-
-RESULT_FILE = "predictions.csv"
-
-@app.post("/upload_predict")
-async def upload_predict(datafile: UploadFile = File(...)):
-from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-import requests
-import pandas as pd
 import io
 
 app = FastAPI()
@@ -72,32 +50,6 @@ async def upload_predict(datafile: UploadFile = File(...)):
 
 @app.get("/download")
 async def download():
-    return FileResponse(
-        path=RESULT_FILE,
-        filename="predictions.csv",
-        media_type="text/csv"
-    )
-    content = await datafile.read()
-
-    response = requests.post(
-        MODEL_URL,
-        files={"file": (datafile.filename, content)}
-    )
-
-    result = response.json()
-
-    labels = result["labels"]
-
-    df = pd.DataFrame({"Label": labels})
-
-    df.to_csv(RESULT_FILE,index=False)
-
-    return {"labels":labels}
-
-
-@app.get("/download")
-async def download():
-
     return FileResponse(
         path=RESULT_FILE,
         filename="predictions.csv",
